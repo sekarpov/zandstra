1b04b320c625026c3ef58ab35966730ea12fdf4c
Sergei Karpov <sergo8ck@gmail.com>
2021-11-16 23:47:25 +0300

    format directories

--- public/dependency_injection.php
+++ public/dependency_injection.php
@@ -5,7 +5,7 @@ use DesignPatterns\DependencyInjection\ObjectAssembler;
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
-$assembler = new ObjectAssembler("src/Patterns/DependencyInjection/objects.xml");
+$assembler = new ObjectAssembler("src/Creational/DependencyInjection/objects.xml");
 $apptmaker = $assembler->getComponent("AppointmentMaker2");
 $out = $apptmaker->makeAppointment();
 print $out;
